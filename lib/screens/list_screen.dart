7320ab82298371d3ffa5eb67fa8b96f7dc87b057
IVAN_RODRIGUEZ_RAMOS <a.20308051281079@cbtis128.edu.mx>
2023-05-16 23:06:10 +0000

    .

---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  var vengadores = ["capitan america", "iron man", "hulk", "thor"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('title'),
+        ),
+        body: ListView(
+          children: [
+            Text("Hola mundo"),
+            Text("6W"),
+            Text("Los vengadores:"),
+            Text(vengadores[0]),
+            Text(vengadores[1]),
+            Text(vengadores[2]),
+            Text(vengadores[3]),
+          ],
+          scrollDirection: Axis.vertical,
+        ));
+  }
+}
